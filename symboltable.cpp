a32791ac53a55dbdd637f5e4bc1a1fe8aee19806
Donald McGilvray Salisbury <dsalisbu@unix2.lt.ucsc.edu>
2015-05-18 22:03:29 -0700

    need to figure out how to handle different blocks I set up the look up function to consider scope as well as fixed some issues with operators and added =

--- symboltable.cpp
+++ symboltable.cpp
@@ -14,6 +14,7 @@ vector<string> attr_name
 size_t next_block = 1;
 symbol_stack* stack = new symbol_stack();
 vector<string> included_filenames;
+bool struct_block = false;
 
 symbol::symbol ( size_t filenr, size_t linenr, size_t offset,
     	size_t blocknr ) :
@@ -24,27 +25,32 @@ symbol::symbol ( size_t filenr, size_t linenr, size_t offset,
 }
 
 void init_symbol_table () {
-	(*stack).push_back(new symbol_table());
+	(*stack).push_back (new symbol_table ());
 }
 
 symbol* lookup (const string* s) {
-	const auto& e = (*stack).front()->find (s);
-	if (e == (*stack).front()->end()) {
-		return NULL;
+
+	auto itor = (*stack).begin ();
+	for (size_t i=0; i<next_block; ++i) {
+		if((*itor) == NULL) continue;
+		const auto& e = (*itor)->find (s);
+		if (e != (*itor++)->end()) {
+			return e->second;
+		}
 	}
-	return e->second;
+	return NULL;
 }
 
-bool attr_or(attr_bitset bits, ... ) {
+bool attr_or (attr_bitset bits, ... ) {
 	va_list attrs;
-	va_start(attrs, bits);
+	va_start (attrs, bits);
 	bool has_attrs = false;
 	size_t flag;
-	while(flag = va_arg(attrs, size_t)) {
+	while (flag = va_arg (attrs, size_t)) {
 		has_attrs = bits[flag];
 		if (has_attrs) break;
 	}
-	va_end(attrs);
+	va_end (attrs);
 	return has_attrs;
 }
 
@@ -79,6 +85,33 @@ string strattr (attr_bitset bits) {
 	return s;
 }
 
+// make if elses before turn in
+string strtype (attr_bitset bits) {
+	string s = "";
+	if(bits[ATTR_typeid]) {
+		s = "typeid";
+	}else{
+	    if(bits[ATTR_int]) s = "int";
+	    if(bits[ATTR_char]) s = "char";
+	    if(bits[ATTR_string]) s = "string";
+	    if(bits[ATTR_bool]) s = "bool";
+	}
+	return s;
+
+}
+
+size_t idtype (attr_bitset bits) {
+	size_t type = 0;
+	if(bits[ATTR_typeid]) {
+		type = ATTR_typeid;
+	}else{
+	    if(bits[ATTR_int]) type = ATTR_int;
+	    if(bits[ATTR_char]) type = ATTR_char;
+	    if(bits[ATTR_string]) type = ATTR_string;
+	    if(bits[ATTR_bool]) type = ATTR_bool;
+	}
+	return type;
+}
 
 bool insert (const string* str, symbol* sym) {
 	if  (!((*stack).front()->insert(make_pair(str, sym))).second) {
@@ -95,6 +128,7 @@ static attr_bitset get_attr(astree* root) {
 	if (lookup(root->lexinfo)) {
 		symbol* sym = lookup(root->lexinfo);
 		root->attr = root->attr | sym->attr;
+		sym->attr = root->attr | sym->attr;
 		return sym->attr;
 	}else if (!(root->attr).to_ulong()){
 	    errprintf ("%:%s: %d: error: unknown identifier : '%s'\n",
@@ -103,6 +137,35 @@ static attr_bitset get_attr(astree* root) {
 		exit(1);
     }
 		return root->attr;
+}
+
+// need to figure out how to handle blocks
+void block (astree* root) {
+	if  (root == NULL) return;
+	if  (struct_block) return;
+	next_block += 1;
+	(*stack).push_back(NULL);
+	(*stack).push_back(new symbol_table());
+}
+
+// write print subtree function for error printing
+void equal_op (astree* root) {
+	if  (root == NULL) return;
+	astree* left = root->children.front();
+	attr_bitset lbits = get_attr(left);
+	astree* right = root->children.back();
+	attr_bitset rbits = get_attr(right);
+	if  (!compat(lbits, rbits)) {
+		errprintf ("%:%s: %d: error: incompatible assignment: type '%s' assigned to type '%s'\n",
+              included_filenames.back().c_str(),
+              root->linenr, strtype(right->attr).c_str(), 
+              strtype(left->attr).c_str());
+		exit(1);
+	}
+	else{
+		right->attr.set(ATTR_vreg, true);
+	}
+		return;
 }
 
 void plus_op (astree* root) {
@@ -116,7 +179,7 @@ void plus_op (astree* root) {
 		root->attr.set(ATTR_int, true);
 		root->attr.set(ATTR_vreg, true);
 	}else{
-		errprintf ("%:%s: %d: incompatible operands : '%s + %s'\n",
+		errprintf ("%:%s: %d: error: incompatible operands: '%s + %s'\n",
               included_filenames.back().c_str(),
               root->linenr, left->lexinfo->c_str(), 
               right->lexinfo->c_str());
@@ -135,7 +198,7 @@ void minus_op (astree* root) {
 		root->attr.set(ATTR_int, true);
 		root->attr.set(ATTR_vreg, true);
 	}else{
-		errprintf ("%:%s: %d: incompatible operands : '%s - %s'\n",
+		errprintf ("%:%s: %d: error: incompatible operands: '%s - %s'\n",
               included_filenames.back().c_str(),
               root->linenr, left->lexinfo->c_str(), 
               right->lexinfo->c_str());
@@ -154,7 +217,7 @@ void mult_op (astree* root) {
 		root->attr.set(ATTR_int, true);
 		root->attr.set(ATTR_vreg, true);
 	}else{
-		errprintf ("%:%s: %d: incompatible operands : '%s * %s'\n",
+		errprintf ("%:%s: %d: error: incompatible operands: '%s * %s'\n",
               included_filenames.back().c_str(),
               root->linenr, left->lexinfo->c_str(), 
               right->lexinfo->c_str());
@@ -173,7 +236,7 @@ void div_op (astree* root) {
 		root->attr.set(ATTR_int, true);
 		root->attr.set(ATTR_vreg, true);
 	}else{
-		errprintf ("%:%s: %d: incompatible operands : '%s / %s'\n",
+		errprintf ("%:%s: %d: error: incompatible operands : '%s / %s'\n",
               included_filenames.back().c_str(),
               root->linenr, left->lexinfo->c_str(), 
               right->lexinfo->c_str());
@@ -181,21 +244,19 @@ void div_op (astree* root) {
 	}
 }
 
-
 void declid (astree* root) {
 	if  (root == NULL) return;
 	if  ((root->symbol == TOK_DECLID) | 
 		(root->symbol == TOK_FIELD) | 
 		(root->symbol == TOK_TYPEID)) {
     	symbol *sym = new symbol(root->filenr, 
-    	root->linenr, root->offset, next_block-1);
+    	    root->linenr, root->offset, next_block-1);
     	sym->attr.set(ATTR_lval, true);
     	if(insert(root->lexinfo, sym)) {
     		root->attr.set(ATTR_lval, true);
     		return;
     	}else {
-    		errprintf ("%:%s: %d: error: redeclaration \
-    			of identifier : '%s'\n",
+    		errprintf ("%:%s: %d: error: redeclaration of identifier : '%s'\n",
             included_filenames.back().c_str(),
             root->linenr, root->lexinfo->c_str());
 		    exit(1);
@@ -207,12 +268,14 @@ void vardecl (astree* root) {
     if  (root == NULL) return;
     if  (compat(root->children.front()->attr, 
     	root->children.back()->attr)) {
-    	printf ("setting %s attr to variable", 
-    		(root->children.front())->children.front()->lexinfo->c_str());
+    	//printf ("setting %s attr to variable", 
+    	//	(root->children.front())->children.front()->lexinfo->c_str());
+    	size_t type = idtype(root->children.back()->attr);
     	astree* grandchild = root->children.front()->children.front();
   		grandchild->attr.set(ATTR_variable, true);
   		symbol * sym = lookup(grandchild->lexinfo);
   		sym->attr.set(ATTR_variable, true);
+  		sym->attr.set(type, true);
     }else{
     	printf ("ERROR NON COMPAT TYPES\n");
     	exit(1);
@@ -334,6 +397,9 @@ func getfunc (int TOK_TYPE) {
 			break;
 		case (TOK_FALSE):
 			foo = boolcon;
+			break;
+		case ('='):
+			foo = equal_op;
 			break;
 		case ('+'):
 			foo = plus_op;
